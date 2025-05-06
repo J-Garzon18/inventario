aaf748ed461ace235c7f25ff658a2ec70ac520dc
J-Garzon18 <alejandro.garzon18164@gmail.com>
2025-05-06 09:01:45 -0500

    estructura basica proyecto express con conexion a base de datos

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+// conexion a mongoo
 const express= require("express")
-const holaRoutes=require("./routes/holaMundo.routes")
-const holaControllers = require("../contoller/holaMundo.controllers")
+const mongoose = require("mongoose")
 require("dotenv").config()
+
+
+
+const holaRoutes=require("./routes/holaMundo")
+const app = express()
 const PORT= process.env.PORT || 3006
-
-router.get("/",holaControllers.holaMundo)
-
-
-const app=express()
-
 
 app.set("port",PORT)
 app.use("/api/hola",holaRoutes)
 
+
 app.get("/",(req,res)=>{
     res.send("Hola Mundo")
 })
+
+mongoose.connect(process.env.MONGO_URI)
+.then(console.log("Base de datos conectada"))
+.catch(err=>console.error("no se pudo conectar a la Base de datos",err))
+
 
 app.listen(PORT,()=>{
     console.log(`Escuchando en el puerto ${PORT}`)
